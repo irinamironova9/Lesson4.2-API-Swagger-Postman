409edf00f9e2c72d2b4fd379ee6caccb6c68e660
Ирина <mi.irene97@gmail.com>
2022-12-11 16:19:05 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/Lesson42ApiSwaggerPostmanApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson42ApiSwaggerPostmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson42ApiSwaggerPostmanApplication.class, args);
+    }
+
+}
